60ad7886d9653de29bab7c98baa1243ab31e1a98
John Pham <johnphammail@gmail.com>
2023-04-10 15:15:01 -0700

    Add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="max-w-lg mx-auto pt-6 px-4">
